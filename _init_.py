f87dadcc33d2a19b68366166c863c66b859796f1
David.Duque <duque1909@javerianacali.edu.co>
2023-11-02 11:10:45 -0500

    Archivo Multiplicacion

--- _init_.py
+++ _init_.py
@@ -1,9 +1,11 @@
-from operations import add
+from operations import add, resta, multiplicacion
 def game():
     score = 0
     while True:
         print('======== Menu ========'
                 '\n1. Add'
+                '\n2. Minus'
+                '\n3. Multiplication'
                 '\n0. Exit')
         option = int(input('\nChoice an option: '))
         if option == 0:
@@ -13,9 +15,19 @@ def game():
         answer = int(input('Enter you answer: '))
         if option == 1:
             result = add(num_1, num_2)
-        if result == answer:
-            score += 1
-            print('Correct!!')
+            if result == answer:
+                score += 1
+                print('Correct!!')
+        if option == 2:
+            result = resta(num_1, num_2)
+            if result == answer:
+                score += 1
+                print('Correct!!')
+        if option == 3:
+            result = multiplicacion(num_1, num_2)
+            if result == answer:
+                score += 2
+                print('Correct!!')
         else:
             print('Incorrect')
     print(f'======== Game Over ========'
--- operations.py
+++ operations.py
@@ -2,3 +2,11 @@ def add(num_1, num_2):
     result = num_1 + num_2
     print(f'{num_1} + {num_2} is equal to {result}')
     return result
+def resta(num_1, num_2):
+    result = num_1 - num_2
+    print(f'{num_1} - {num_2} is equal to {result}')
+    return result
+def multiplicacion(num_1, num_2):
+    result = num_1 * num_2
+    print(f'{num_1} * {num_2} is equal to {result}')
+    return result
